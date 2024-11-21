fcfad6bff4eab020ee3a41b9c3ab9d77e031d58f
Rizon <kye021@hotmail.co.uk>
2024-11-21 20:27:13 +0000

    For Connor to fill in

--- src/app/(pages)/about/components/aboutus/AboutUsC.tsx
+++ src/app/(pages)/about/components/aboutus/AboutUsC.tsx
@@ -27,9 +27,9 @@ export const AboutUs: FC = () => {
             viewport={{ amount: 0.2, once: true }}
             className={s.Header}
           >
-            <motion.p variants={animation} custom={3} className="text-gray">
+            <motion.h2 variants={animation} custom={3} className="text-white">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </motion.p>
+            </motion.h2>
           </motion.section>
         </section>
       </div>
