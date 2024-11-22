252c104beef1d7cb5c45a61c6f4f0ad57a46f62d
TheRealToxicDev <toxic.dev09@gmail.com>
2024-11-22 01:44:16 -0700

    feat(test): seo generation fixes

--- src/app/(pages)/status/page.tsx
+++ src/app/(pages)/status/page.tsx
@@ -2,8 +2,25 @@ import { StatusLayout } from './layouts/Status';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Status',
+    title: "Status",
     description: 'Check the status of our services and any ongoing maintenance.',
+    openGraph: {
+        title: 'NodeByte Hosting',
+        description: 'Check the status of our services and any ongoing maintenance.',
+        url: 'https://nodebyte.host/status',
+        siteName: 'NodeByte Hosting',
+        locale: 'en_US',
+        type: 'website',
+    },
+    twitter: {
+        title: 'NodeByte Hosting',
+        description: 'Check the status of our services and any ongoing maintenance.',
+        images: 'https://nodebyte.host/banner.png',
+        card: 'summary_large_image'
+    },
+    alternates: {
+        canonical: 'https://nodebyte.host/status'
+    }
 };
 
 
